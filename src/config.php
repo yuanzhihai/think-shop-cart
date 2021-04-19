a7ced3d505f222b0f6c1def7fb1576c230c220e8
dtid_4cb73ba1d0cd43af <396751927@qq.com>
2021-04-19 12:26:00 +0800

    init

---
+++ src/config.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * This file is part of ibrand/laravel-shopping-cart.
+ *
+ * (c) iBrand <https://www.ibrand.cc>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'storage' => '\yzh52521\ShoppingCart\storage\SessionStorage::class',
+    'table'   => 'shopping_cart',
+];
